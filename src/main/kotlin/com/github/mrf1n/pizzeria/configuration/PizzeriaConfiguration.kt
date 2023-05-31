3a59bc6d97354295d472c758ff8398a046245a09
Sergey <mrf1n@github.com>
2023-05-31 13:06:46 +0200

    On property condition was added for ToppingsService implementations

--- src/main/kotlin/com/github/mrf1n/pizzeria/configuration/PizzeriaConfiguration.kt
+++ src/main/kotlin/com/github/mrf1n/pizzeria/configuration/PizzeriaConfiguration.kt
@@ -1,8 +1,10 @@
 package com.github.mrf1n.pizzeria.configuration
 
 import com.github.mrf1n.pizzeria.repository.CustomerRepository
+import com.github.mrf1n.pizzeria.service.InMemoryToppingsService
 import com.github.mrf1n.pizzeria.service.RepositoryToppingsService
 import com.github.mrf1n.pizzeria.service.ToppingsService
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 
@@ -10,9 +12,16 @@ import org.springframework.context.annotation.Configuration
 class PizzeriaConfiguration {
 
     @Bean
-    fun toppingsService(customerRepository: CustomerRepository): ToppingsService =
-        RepositoryToppingsService(customerRepository)
+    @ConditionalOnProperty(
+        prefix = "pizzeria.storage",
+        value = ["type"],
+        havingValue = "inmemory",
+        matchIfMissing = true
+    )
+    fun toppingsServiceInMemory(): ToppingsService = InMemoryToppingsService()
 
-//    @Bean
-//    fun toppingsService(): ToppingsService = InMemoryToppingsService()
+    @Bean
+    @ConditionalOnProperty(prefix = "pizzeria.storage", value = ["type"], havingValue = "repository")
+    fun toppingsServiceRepository(customerRepository: CustomerRepository): ToppingsService =
+        RepositoryToppingsService(customerRepository)
 }
