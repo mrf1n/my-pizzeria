e32b5e72182552152b6ca8a3c710a20be38bf0cc
Sergey <mrf1n@github.com>
2023-05-31 15:16:46 +0200

    Fix for LazyInitializationException without Eager fetch type

--- src/main/kotlin/com/github/mrf1n/pizzeria/model/Customer.kt
+++ src/main/kotlin/com/github/mrf1n/pizzeria/model/Customer.kt
@@ -2,13 +2,12 @@ package com.github.mrf1n.pizzeria.model
 
 import jakarta.persistence.ElementCollection
 import jakarta.persistence.Entity
-import jakarta.persistence.FetchType
 import jakarta.persistence.Id
 
 @Entity
 data class Customer(
     @Id
     val email: String,
-    @ElementCollection(fetch = FetchType.EAGER)
+    @ElementCollection
     val toppings: Set<String> = setOf()
 )
--- src/main/kotlin/com/github/mrf1n/pizzeria/repository/CustomerRepository.kt
+++ src/main/kotlin/com/github/mrf1n/pizzeria/repository/CustomerRepository.kt
@@ -3,7 +3,9 @@ package com.github.mrf1n.pizzeria.repository
 import com.github.mrf1n.pizzeria.model.Customer
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.data.jpa.repository.Query
+import org.springframework.data.repository.query.Param
 import org.springframework.stereotype.Repository
+import java.util.*
 
 @Repository
 interface CustomerRepository : JpaRepository<Customer, String> {
@@ -12,4 +14,7 @@ interface CustomerRepository : JpaRepository<Customer, String> {
     fun findAllToppings(): List<String>
 
     fun countByToppingsContaining(topping: String): Int
+
+    @Query("SELECT c FROM Customer c JOIN FETCH c.toppings WHERE c.id = :id")
+    fun findByIdWithToppings(@Param("id") id: String): Optional<Customer>
 }
